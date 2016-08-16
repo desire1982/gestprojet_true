ce7286e3d247b319d1421be559b85571a7cf1b7b
Desire Kouame <kouamedesire88@gmail.com>
2016-08-16 17:03:17 +0000

    Page connexion

--- index1.php
+++ index1.php
@@ -1,55 +1,124 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
+<?php 
+//include_once('config/connectmysql.php');
+ob_start();
+ session_start();
+ require_once 'config/connectmysql.php';
+ 
+ // it will never let you open index(login) page if session is set
+ if ( isset($_SESSION['user'])!="" ) {
+  header("Location: acceuil.php");
+  exit;
+ }
+ 
+ if( isset($_POST['btn-login']) ) { 
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+	$ip=$_SERVER["REMOTE_ADDR"];
+	$date_connexion = date('Y-m-d H:i:s');
+  
+  
+  $email = $_POST['email'];
+  $upass = $_POST['pass'];
+  
+  
+  $email = strip_tags(trim($email));
+  $upass = strip_tags(trim($upass));
+  
+  $password = hash('sha256', $upass); // password hashing using SHA256
+  
+  $res=mysql_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
+  
+  $row=mysql_fetch_array($res);
+  
+  $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
+  
+  if( $count == 1 && $row['userPass']==$password ) {
+   $_SESSION['user'] = $row['userId'];
+   header("Location: home.php");
+  } else {
+   $errMSG = "Wrong Credentials, Try again...";
+  }
+ }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Signin Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
+    <title>Gestion Projet</title>
+ 
+    <!-- Bootstrap -->
     <link href="module/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coding Cage - Login & Registration System</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
 
-    <!-- Custom styles for this template -->
-    <link href="css/signin.css" rel="stylesheet">
+<div class="container">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <div class="container">
-
-      <form class="form-signin">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
+ <div id="login-form">
+    <form method="post" autocomplete="off">
+    
+     <div class="col-md-12">
+        
+         <div class="form-group">
+             <h2 class="">Sign In.</h2>
+            </div>
+        
+         <div class="form-group">
+             <hr />
+            </div>
+            
+            <?php
+   if ( isset($errMSG) ) {
+    
+    ?>
+    <div class="form-group">
+             <div class="alert alert-danger">
+    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+             </div>
+                <?php
+   }
+   ?>
+            
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+             <input type="text"  id="username" name="username" class="form-control" placeholder="Votre nom d'utilisateur" required />
+                </div>
+            </div>
+            
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+             <input type="password" name="pass" class="form-control" placeholder="Votre mot de passe" required />
+                </div>
+            </div>
+            
+            <div class="form-group">
+             <hr />
+            </div>
+            
+            <div class="form-group">
+             <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
+            </div>
+            
+            <div class="form-group">
+             <hr />
+            </div>
+            
+            <div class="form-group">
+             <a href="register.php">Sign Up Here...</a>
+            </div>
+        
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
+   
+    </form>
+    </div> 
 
-    </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+</div>
